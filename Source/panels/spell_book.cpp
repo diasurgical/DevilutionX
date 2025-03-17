4f50e17d7176ed6800a3dd4c03e5d4735a161a6c
Eric Robinson <ericjrobinson@live.com>
2025-03-17 16:07:55 -0400

    Use Starting Loadout table for Spell Book

--- Source/panels/spell_book.cpp
+++ Source/panels/spell_book.cpp
@@ -89,7 +89,7 @@ void PrintSBookStr(const Surface &out, Point position, std::string_view text, Ui
 SpellType GetSBookTrans(SpellID ii, bool townok)
 {
 	Player &player = *InspectPlayer;
-	if ((player._pClass == HeroClass::Monk) && (ii == SpellID::Search))
+	if (ii == GetPlayerStartingLoadoutForClass(InspectPlayer->_pClass).skill)
 		return SpellType::Skill;
 	SpellType st = SpellType::Spell;
 	if ((player._pISpells & GetSpellBitmask(ii)) != 0) {
