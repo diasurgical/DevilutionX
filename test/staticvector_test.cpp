faa786dcddfda34276b595fd6d60727afa6cc1a7
Yggdrasill <yggdrasill@shitpost.is>
2025-03-24 13:35:33 +0000

    Implement tests for StaticVector
    Style fixups

---
+++ test/staticvector_test.cpp
@@ -0,0 +1,53 @@
+#include <cstdlib>
+#include <ctime>
+#include <gtest/gtest.h>
+
+#include "utils/static_vector.hpp"
+
+#define MAX_SIZE 256
+
+using namespace devilution;
+
+namespace {
+StaticVector<int, MAX_SIZE> container;
+
+TEST(StaticVector, StaticVector_push_back)
+{
+	std::srand(clock());
+	size_t size = std::rand() % MAX_SIZE;
+	for (size_t i = 0; i < size; i++) {
+		container.push_back(i);
+	}
+
+	for (size_t i = 0; i < container.size(); i++) {
+		EXPECT_EQ(container[i], i);
+	}
+}
+
+TEST(StaticVector, StaticVector_erase)
+{
+	std::srand(clock());
+	int erasures = std::rand() % container.size();
+
+	std::vector<int> erase_idx;
+	std::vector<int> expected;
+
+	for (int i = 0; i < erasures; i++) {
+		erase_idx.push_back(std::rand() % container.size());
+	}
+
+	for (int i = 0; expected.size() < container.size(); i++) {
+		expected.push_back(i);
+	}
+
+	for (int idx : erase_idx) {
+		container.erase(container.begin() + idx, container.begin() + idx + 1);
+		expected.erase(expected.begin() + idx);
+	}
+
+	for (size_t i = 0; i < expected.size(); i++) {
+		EXPECT_EQ(container[i], expected[i]);
+	}
+}
+
+} // namespace
