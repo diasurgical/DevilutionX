720b8cc3464399fa5d4543232ea382fa267ea145
Anders Jenbo <anders@jenbo.dk>
2025-09-04 23:15:55 +0200

    Remove unused header charconv

--- Source/itemdat.cpp
+++ Source/itemdat.cpp
@@ -6,7 +6,6 @@
 
 #include "itemdat.h"
 
-#include <charconv>
 #include <string_view>
 #include <vector>
 
@@ -729,3 +728,4 @@ std::string_view ItemTypeToString(ItemType itemType)
 }
 
 } // namespace devilution
+
