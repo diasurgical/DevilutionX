f3915a4ef3e1631ebf61c2b480fd32855b49edff
Eric Robinson <ericjrobinson@live.com>
2025-12-10 16:22:01 -0500

    Dummy commit to trigger tests

--- Source/player.cpp
+++ Source/player.cpp
@@ -2399,7 +2399,7 @@ void NextPlrLevel(Player &player)
 
 	if (&player == MyPlayer) {
 		RedrawComponent(PanelDrawComponent::Health);
-	}
+	}fssgDGsg
 
 	const int mana = player.getClassAttributes().lvlMana;
 
